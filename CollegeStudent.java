7292f1732d52335079a0655f9c7c4d18ca416707
ashtonpenman <88688089+ashtonpenman@users.noreply.github.com>
2025-12-12 01:35:00 +0000

    Im done but my scanner doesnt work at the end of my while loop, I think its stupid

--- CollegeStudent.java
+++ CollegeStudent.java
@@ -1,2 +1,79 @@
 // Create your CollegeStudent class here
+public class CollegeStudent{
+    private static int nextStudentID = 1;
+    private int studentID;
+    private String name;
+    private String courseTitle;
+    private String courseNumber;
+    private static int costPerCredit = 125;
+    private int credits;
+    private int courseFee;
 
+    public CollegeStudent() {
+        studentID = nextStudentID;
+        nextStudentID++;
+    }
+
+    public CollegeStudent(String initName, String initCourseTitle, String initCourseNumber, int initCredits) {
+        name = initName;
+        courseTitle = initCourseTitle;
+        courseNumber = initCourseNumber;
+        credits = initCredits;
+        courseFee = credits * costPerCredit;
+        studentID = nextStudentID;
+        nextStudentID++;
+    }
+
+    public void setName(String initName){
+        name = initName;
+    }
+
+    public void setCourseTitle(String initcourseTitle){
+        courseTitle = initcourseTitle;
+    }
+
+    public void setCourseNumber(String initCourseNumber){
+        courseNumber = initCourseNumber;
+    }
+
+    public void setCredits(int initCredits){
+        credits = initCredits;
+        courseFee = credits * costPerCredit;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getCourseTitle(){
+        return courseTitle;
+    }
+
+    public String getCourseNumber(){
+        return courseNumber;
+    }
+
+    public int getCredits(){
+        return credits;
+    }
+
+    public int getCourseFee(){
+        return courseFee;
+    }
+
+    public int getStudentID(){
+        return studentID;
+    }
+
+    public void print(){
+        System.out.println("Name: " + name);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Course Title: " + courseTitle);
+        System.out.println("Course Number: " + courseNumber);
+        System.out.println("Credits: " + credits);
+        System.out.println("Course Fee: $" + courseFee);
+    }
+
+
+
+}
--- Main.java
+++ Main.java
@@ -6,13 +6,29 @@ class Main {
 
     Scanner scan = new Scanner(System.in);
     
-    // write a while loop to create and display CollegeStudnets using the below method
-    
-  }
+    // write a while loop to create and display CollegeStudents using the below method
+    String moreStudents = "Y";
+    while(moreStudents.equals("Y")){
+      CollegeStudent S1 = createStudent(scan);
+      S1.print();
+      System.out.println("Would you like to make another student? (Y/N)");
+      moreStudents = scan.nextLine();
+
+      }
+      scan.close();
+    }
 
   private static CollegeStudent createStudent(Scanner scan) {
     // complete this method to create a CollegeStudent object with details provided by the user
-    
+    System.out.println("What is the students name? ");
+    String newName = scan.nextLine();
+    System.out.println("What class is the student taking? ");
+    String newCourseTitle = scan.nextLine();
+    System.out.println("What number is this class? ");
+    String newCourseNumber = scan.nextLine();
+    System.out.println("How many credits is this course?");
+    int newCredits = scan.nextInt();
+    return new CollegeStudent(newName, newCourseTitle, newCourseNumber, newCredits);
   }
     
 }
